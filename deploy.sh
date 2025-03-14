066e968785adaf9d26082030019e6e50dea3af07
Dmitry Kuskov <dmtrkskv@gmail.com>
2025-03-14 18:25:15 +0300

    chore: добавлен pull и установка зависимостей в скрипт deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -e
 
+git pull 
+
+echo "Installing dependencies in the root directory..."
+yarn install
+
+echo "Installing dependencies in the /backend directory..."
+cd backend
+yarn install
+cd ..
+
+echo "Installing dependencies in the /frontend directory..."
+cd frontend
+yarn install
+cd ..
+
 yarn build
 sh ./backend/deploy-backend.sh
