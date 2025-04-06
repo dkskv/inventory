65c649e11acf61526108392d1bb5e0c4924bc147
Dmitry Kuskov <dmtrkskv@gmail.com>
2025-04-06 23:21:33 +0300

    chore: исправлены команды для бэкапа базы данных (переменные подставлялись без экранирования)

--- backend/backup-scripts/backup.sh
+++ backend/backup-scripts/backup.sh
@@ -10,7 +10,9 @@ BACKUP_FILE="$BACKUP_DIR/backup-${DB_NAME}-$TIMESTAMP.dump"
 # Создаем папку, если её нет
 docker exec "$CONTAINER_NAME" mkdir -p "$BACKUP_DIR"
 
-docker exec "$CONTAINER_NAME" sh -c "PGPASSWORD=$DB_PASSWORD pg_dump -U $DB_USER -d $DB_NAME -F c -f $BACKUP_FILE"
+docker exec "$CONTAINER_NAME" \
+  env PGPASSWORD="$DB_PASSWORD" \
+  pg_dump -U "$DB_USER" -d "$DB_NAME" -F c -f "$BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
   echo "✅ Бэкап успешно сохранен в контейнере: $BACKUP_FILE"
